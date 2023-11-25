b35addf7175f79b0d40e1f2238ccacb136dbab63
Kaguyasuo <williamkhorton@gmail.com>
2023-11-25 16:29:58 -0500

    login ui complete
    - menu bar added, url functionality between web pages added, create account and add account pages

--- finalProject/login/urls.py
+++ finalProject/login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact, name='login')
+    path('1/', views.login, name='login'),
+    path('2/', views.create_account, name='create-account')
 ]
--- finalProject/login/views.py
+++ finalProject/login/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from ticketmaster import views
+from ticketmaster.views import ticketmaster
 from .forms import ContactForm
 
 
-def contact(request):
+def login(request):
     # Create a form instance and populate it with data from the request if request is a POST (request.POST parts works)
     # if not, create an empty form instance (None parts works below)
     form = ContactForm(request.POST or None)
-
     # Check to see if request is a POST
     # if this is a POST request, we need to process the form data and save the data
     if request.method == 'POST':
@@ -16,7 +18,24 @@ def contact(request):
             # print (form.cleaned_data['email']) # for example,email can be accessed using form.cleaned_data['email']
             form.save()
             # redirect to a new URL:
-            return render(request, 'login/thanks.html')
+            return redirect('/ticketmaster/')
+            # return render(request, 'ticketmaster/index.html')
 
     context = {'form': form}
-    return render(request, 'login/form.html', context)
+    return render(request, 'login/login-page.html', context)
+
+
+def create_account(request):
+    form = ContactForm(request.POST or None)
+    # Check to see if request is a POST
+    # if this is a POST request, we need to process the form data and save the data
+    if request.method == 'POST':
+        # before saving check whether the form data is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # print (form.cleaned_data['email']) # for example,email can be accessed using form.cleaned_data['email']
+            form.save()
+            # redirect to a new URL:
+            return redirect('/ticketmaster/')
+    context = {'form': form}
+    return render(request, 'login/create-account-page.html', context)
