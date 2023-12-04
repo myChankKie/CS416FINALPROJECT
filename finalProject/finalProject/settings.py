1fdb852d1fb0d72da6b790dcdb537d51854cbd1a
Kaguyasuo <williamkhorton@gmail.com>
2023-12-04 15:38:41 -0500

    update

--- finalProject/finalProject/settings.py
+++ finalProject/finalProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,8 +122,13 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = ['static']
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ['static']
+# STATIC_ROOT = BASE_DIR / 'static'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
