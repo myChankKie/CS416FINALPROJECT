46db04a8e1ff0fb80813607362a53d729112de97
Kaguyasuo <williamkhorton@gmail.com>
2023-12-04 15:09:48 -0500

    cloud deployment debugging

--- finalProject/finalProject/settings.py
+++ finalProject/finalProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'login.apps.ContactConfig',
     'ticketmaster.apps.TicketmasterConfig',
     'crispy_forms',
-    "crispy_bootstrap5"
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
